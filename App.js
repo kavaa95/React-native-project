4285dbdf7106d8d8bae81ae586bacb6f3eb18e76
kavaa95 <alexkavanagh83@yahoo.com>
2022-02-01 18:20:24 -0500

    testing to to diferent devices

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
 import ViewImageScreen from "./app/Screens/viewImageScreen";
 import WelcomeScreen from "./app/Screens/welcomeScreen";
+import AppText from "./app/components/AppText";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <AppText />;
 }
---
+++ app/components/AppText.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  Platform,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+export default function AppText({ children }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello</Text>
+      <AntDesign name="mail" size={24} color="black" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "tomato",
+    ...Platform.select({
+      ios: {
+        fontSize: 60,
+        fontFamily: "Avenir",
+      },
+      android: {
+        fontSize: 38,
+        fontFamily: "Roboto",
+      },
+    }),
+  },
+});
