669c20f64936d000c8be3977b78e8e14f99f97aa
kavaa95 <alexkavanagh83@yahoo.com>
2022-02-02 00:55:38 -0500

    we finish the details listing components

--- app/Screens/ListingDetailsScreen.js
+++ app/Screens/ListingDetailsScreen.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { View, Image, StyleSheet } from "react-native";
+import AppText from "../components/AppText/AppText";
+import colors from "../config/colors";
 
 function ListingDetailsScreen(props) {
   return (
-    <View source={require("../assets/jacket.jpg")} style={styles.image}></View>
+    <View>
+      <Image source={require("../assets/jacket.jpg")} style={styles.image} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Red Jacket for sale</AppText>
+        <AppText style={styles.price}>$100</AppText>
+      </View>
+    </View>
   );
 }
 
@@ -11,6 +19,19 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: 300,
+  },
+  detailsContainer: {
+    padding: 20,
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: "bold",
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 500,
   },
 });
 
