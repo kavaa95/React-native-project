21cd3ec001e106c589e86159017c4c41bf551cda
kavaa95 <alexkavanagh83@yahoo.com>
2022-02-02 00:57:56 -0500

    add protrait image

--- app/Screens/ListingDetailsScreen.js
+++ app/Screens/ListingDetailsScreen.js
@@ -10,6 +10,7 @@ function ListingDetailsScreen(props) {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red Jacket for sale</AppText>
         <AppText style={styles.price}>$100</AppText>
+        <ListItem image={require("../assets")} />
       </View>
     </View>
   );
