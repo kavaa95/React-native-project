6520fe5ba957d2aa56f269004ed9d9f43cdad071
kavaa95 <alexkavanagh83@yahoo.com>
2022-02-02 09:42:28 -0500

    install expo-constant

--- app/Screens/MessageScreen.js
+++ app/Screens/MessageScreen.js
@@ -1,7 +1,16 @@
 import React from "react";
-import { FlatList, StyleSheet, View, Text } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+  Image,
+  Platform,
+} from "react-native";
 import ListItem from "../components/ListItem";
-
+import { Constants } from "expo-constants";
 const messages = [
   {
     id: 1,
@@ -17,20 +26,26 @@ const messages = [
   },
 ];
 
-function MessageScreen(props) {
+export default function MessageScreen() {
   return (
-    <FlatList
-      data={messages}
-      keyExtractor={(message) => message.id.toString}
-      renderItem={({ item }) => (
-        <ListItem
-          title={items.title}
-          subTitle={item.description}
-          image={item.image}
-        />
-      )}
-    />
+    <SafeAreaView style={styles.screen}>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+          />
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
-export default MessageScreen;
+const styles = StyleSheet.create({
+  screen: {
+    paddingTop: Constants.StatusBarHeight,
+  },
+});
