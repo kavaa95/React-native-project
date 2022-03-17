d33d02f5202a033ad7b758eec1e02eb5495e5b97
kavaa95 <alexkavanagh83@yahoo.com>
2022-03-17 03:45:06 -0400

    implement image picker

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import {
   ImageBackground,
   TextInput,
   Switch,
+  Platform,
+  Button,
 } from "react-native";
 import ViewImageScreen from "./app/Screens/viewImageScreen";
 import WelcomeScreen from "./app/Screens/welcomeScreen";
@@ -25,7 +27,34 @@ import AppTextInput from "./app/components/AppTextInput";
 import AppPicker from "./app/components/AppPicker";
 import LoginScreen from "./app/Screens/LoginScreen";
 import ListingEditScreen from "./app/Screens/ListingEditScreen";
+import { useEffect } from "react/cjs/react.production.min";
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
-  return <ListingEditScreen />;
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    // console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && (
+        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+      )}
+    </View>
+  );
 }
